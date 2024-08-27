2d92e4adb29aa80e6cd593d19c0ba898eb9035e1
PanGami <pancanugrahawicaksana@gmail.com>
2024-08-27 08:51:18 +0700

    little change

--- entity/user.go
+++ entity/user.go
@@ -1,14 +1,10 @@
 package entity
 
-import "time"
-
 type User struct {
 	ID         int32          `gorm:"primaryKey;autoIncrement" json:"id"`
 	Username   string         `gorm:"size:255;not null;unique" json:"username"`
-	FullName   string         `gorm:"size:255;not null;unique" json:"fullName"`
+	FullName   string         `gorm:"size:255;not null" json:"full_name"`
 	Password   string         `gorm:"size:255;not null" json:"password"`
-	CreatedAt  time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt  time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 	Activities []UserActivity `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"activities"`
 }
 
